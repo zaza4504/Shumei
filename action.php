3d13cd739a7cd16be5033b1f28a8100108b73106
zaza4504 <zaza4504@163.com>
2016-04-29 21:21:45 +0300

    Latest version of index.html and action.php

--- action.php
+++ action.php
@@ -1,8 +1,8 @@
 <?php
-if( isset($_POST['TwitterAccount'])) {
-    $data = $_SERVER['REMOTE_ADDR'] . "\n" . $_POST['TwitterAccount'] . "\n";
-    #$ret = file_put_contents('/Users/secoder/Documents/SGN-35016_Internet_of_Thing/mydata.txt', $data, FILE_APPEND | LOCK_EX);
-    $ret = file_put_contents('./mydata.txt', $data, LOCK_EX);
+if(isset($_POST['MACAddress']) && isset($_POST['TwitterAccount'])) {
+    $data = $_SERVER['REMOTE_ADDR'] . "\n" . $_POST['MACAddress'] . "\n" . $_POST['TwitterAccount'] . "\n";
+    #$ret = file_put_contents('/home/pi/Projects/IoT/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('/home/pi/Projects/IoT/mydata.txt', $data, LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
