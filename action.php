2ef14f14a5a82d338049ae9b22345606683b3e0f
zaza4504 <zaza4504@163.com>
2016-05-08 17:06:15 +0300

    Refine the config page. Ignore the last commit

--- action.php
+++ action.php
@@ -1,15 +1,35 @@
 <?php
-if(isset($_POST['MACAddress']) && isset($_POST['TwitterAccount'])) {
-    $data = $_SERVER['REMOTE_ADDR'] . "\n" . $_POST['MACAddress'] . "\n" . $_POST['TwitterAccount'] . "\n";
-    #$ret = file_put_contents('/home/pi/Projects/IoT/mydata.txt', $data, FILE_APPEND | LOCK_EX);
-    $ret = file_put_contents('/home/pi/Projects/IoT/mydata.txt', $data, LOCK_EX);
+if( isset($_POST['demo']) && isset($_POST['TwitterAccount']) ) {
+    $id = $_POST['TwitterAccount'] . "\n";
+    $personality = $_POST['demo'] . "\n";
+    $ip = $_SERVER['REMOTE_ADDR'] . "\n";
+
+    echo "Your personality:  {$personality} <br><br>";
+    $ret = file_put_contents('/home/pi/Projects/IoT/id', $id, LOCK_EX);
     if($ret === false) {
-        die('There was an error writing this file');
-    }
+        die('There was an error writing id file');
+    }   
     else {
-        echo "\nConfiguration is Done. Enjoy! :)";
+        echo "Save your Twitter ID<br>";
     }
-}
+   
+    $ret = file_put_contents('/home/pi/Projects/IoT/personality', $personality, LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing personality file');
+    }   
+    else {
+        echo "Save your personality<br>";
+    }   
+
+    $ret = file_put_contents('/home/pi/Projects/IoT/ip', $ip, LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing ip file');
+    }   
+    else {
+        echo "Save your IP<br><br>";
+        echo "Configuration is done. Enjoy! :)<br>";
+    }   
+}   
 else {
    die('no post data to process');
-}
+} 
